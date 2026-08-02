4bcc54921689d9747ec0556e2e3c1dfff93eac98
Minh Hieu <minhhieu1304202@gmail.com>
2026-08-02 18:03:47 +0700

    Bo admin, cau hinh ALLOWED_HOSTS va DEBUG cho production

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
